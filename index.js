55e78a3085ec6a1c80d4e65215310de2a27d9667
Emre Unal <emreunal99@gmail.com>
2014-10-26 21:03:44 +0200

    Fixes #2, check missing deps on init

--- index.js
+++ index.js
@@ -2,54 +2,67 @@
 
 var path = require('path');
 var fs   = require('fs');
-var sys  = require('sys');
 var EOL  = require('os').EOL;
 
-module.exports = {
-  name: 'ember-cli-dependency-checker',
-  included: function(app) {
-    this._super.included.apply(this, arguments);
+var alreadyPrinted = false;
 
-    var bowerDeps = this.readBowerFileDependencies(app);
-    var bowerDirectoryContents = this.readBowerDirectoryContents(app);
-    var missingBowerDeps = this.diff(bowerDeps, bowerDirectoryContents);
+function EmberCLIDependencyChecker(project) {
+  this.name    = 'ember-cli-dependency-checker';
+  this.project = project;
+  this.checkDependencies(project);
+}
 
-    var pkgDeps = this.readPackageFileDependencies(app);
-    var modulesDirectoryContents = this.readModulesDirectoryContents(app);
-    var missingNodeDeps = this.diff(pkgDeps, modulesDirectoryContents);
+var diff = function(first, second) {
+  return first.filter(function(i) {
+    return second.indexOf(i) < 0;
+  });
+};
 
+EmberCLIDependencyChecker.prototype.checkDependencies = function(project) {
+  var bowerDeps = this.readBowerFileDependencies(project);
+  var bowerDirectoryContents = this.readBowerDirectoryContents(project);
+  var missingBowerDeps = diff(bowerDeps, bowerDirectoryContents);
+
+  var pkgDeps = this.readPackageFileDependencies(project);
+  var modulesDirectoryContents = this.readModulesDirectoryContents(project);
+  var missingNodeDeps = diff(pkgDeps, modulesDirectoryContents);
+
+  if(!alreadyPrinted) {
     this.printMissingDependencies('bower', missingBowerDeps);
     this.printMissingDependencies('npm', missingNodeDeps);
-  },
-  readModulesDirectoryContents: function(app) {
-    var modulesDirectory = path.join(app.project.root, 'node_modules');
-    return fs.readdirSync(modulesDirectory);
-  },
-  readBowerDirectoryContents: function(app) {
-    var bowerDirectory = path.join(app.project.root, app.bowerDirectory);
-    return fs.readdirSync(bowerDirectory);
-  },
-  readBowerFileDependencies: function(app) {
-    var bowerFilePath = path.join(app.project.root, 'bower.json');
-    var bowerFileContents = fs.readFileSync(bowerFilePath);
-    var bower = JSON.parse(bowerFileContents);
-    return Object.keys(bower.dependencies);
-  },
-  readPackageFileDependencies: function(app) {
-    return Object.keys(app.project.dependencies());
-  },
-  printMissingDependencies: function(type, dependencies) {
-    if(dependencies.length > 0) {
-      sys.print(EOL + 'Missing ' + type + ' dependencies: ' + EOL);
-      dependencies.forEach(function(name) {
-        sys.print('\t' + name + EOL);
-      });
-      sys.print(EOL);
-    }
-  },
-  diff: function(first, second) {
-    return first.filter(function(i) {
-      return second.indexOf(i) < 0;
-    });
+    alreadyPrinted = true;
   }
 };
+
+EmberCLIDependencyChecker.prototype.readModulesDirectoryContents = function(project) {
+  var modulesDirectory = path.join(project.root, 'node_modules');
+  return fs.readdirSync(modulesDirectory);
+};
+
+EmberCLIDependencyChecker.prototype.readBowerDirectoryContents = function(project) {
+  var bowerDirectory = path.join(project.root, project.bowerDirectory);
+  return fs.readdirSync(bowerDirectory);
+};
+
+EmberCLIDependencyChecker.prototype.readBowerFileDependencies = function(project) {
+  var bowerFilePath = path.join(project.root, 'bower.json');
+  var bowerFileContents = fs.readFileSync(bowerFilePath);
+  var bower = JSON.parse(bowerFileContents);
+  return Object.keys(bower.dependencies);
+};
+
+EmberCLIDependencyChecker.prototype.readPackageFileDependencies = function(project) {
+  return Object.keys(project.dependencies());
+};
+
+EmberCLIDependencyChecker.prototype.printMissingDependencies = function(type, dependencies) {
+  if(dependencies.length > 0) {
+    process.stdout.write(EOL + 'Missing ' + type + ' dependencies: ' + EOL);
+    dependencies.forEach(function(name) {
+      process.stdout.write('\t' + name + EOL);
+    });
+    process.stdout.write(EOL);
+  }
+};
+
+module.exports = EmberCLIDependencyChecker;
