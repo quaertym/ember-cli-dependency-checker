d3d637e94a04b9574c56a018c6aac7f5f1a4163d
Estelle DeBlois <edeblois@gmail.com>
2014-10-31 17:09:43 -0400

    Add dependency checker tests
    * Move `index.js` to `lib/dependency-checker`
    * Add jshint for `lib` and `tests`
    * Remove Ember App related boilerplate in `tests` since it doesn't apply to this addon
    * Add unit tests for NPM
    * Add placeholder test file for Bower

---
+++ tests/helpers/assert-error.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var assert            = require('chai').assert;
+var DependencyChecker = require('../../lib/dependency-checker');
+var DependencyError   = require('../../lib/dependency-error');
+
+module.exports = function(project, type) {
+  var dependencyChecker = function() {
+    return new DependencyChecker(project);
+  };
+
+  assert.throws(dependencyChecker, DependencyError, 'Missing ' + type + ' packages');
+};
