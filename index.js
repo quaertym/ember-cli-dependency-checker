192d5e1b74472597654c710cce31aac9df82a1ac
Matthew Beale <matt.beale@madhatted.com>
2015-05-14 12:25:55 -0400

    Use init instead of a bare constructor

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 var Reporter = require('./lib/reporter');
 var DependencyChecker = require('./lib/dependency-checker');
 
-module.exports = function emberCliDependencyCheckerAddon(project) {
-  var reporter = new Reporter();
-  var dependencyChecker = new DependencyChecker(project, reporter);
-  dependencyChecker.checkDependencies();
-
-  return {
-    name: 'ember-cli-dependency-checker'
-  };
+module.exports = {
+  name: 'ember-cli-dependency-checker',
+  init: function() {
+    var reporter = new Reporter();
+    var dependencyChecker = new DependencyChecker(this.project, reporter);
+    dependencyChecker.checkDependencies();
+  }
 };
