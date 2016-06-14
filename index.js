6c05a3316fde10b502baec5555502fd7b3212adf
Stefan Penner <stefan.penner@gmail.com>
2016-06-14 09:56:23 -0700

    CoreObject 1.x and 2.x Compat

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var DependencyChecker = require('./lib/dependency-checker');
 module.exports = {
   name: 'ember-cli-dependency-checker',
   init: function() {
+    this._super.init && this._super.init.apply(this, arguments);
+    
     var reporter = new Reporter();
     var dependencyChecker = new DependencyChecker(this.project, reporter);
     dependencyChecker.checkDependencies();
